0e6fae11876b1146649e078237f754bc4381a1cc
Ton Nom <ton@email.com>
2026-05-11 11:13:07 +0200

    test8

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "test Hello EPSIC - DevOps - ALED"
+    return "test test 2 Hello EPSIC - DevOps - ALED"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
